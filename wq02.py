fc7985dc5e44750865aea2d65cd573ac4dba4964
Wangqian550 <51935916+Wangqian550@users.noreply.github.com>
2019-06-22 18:18:23 +0800

    Update wq02.py

--- wq02.py
+++ wq02.py
@@ -13,15 +13,3 @@ while 1:
     if (response.status != 200):
         print('爬虫失败')
     break
-
-
-
-    import http.cookiejar,urllib.request
-    cookie = http.cookiejar.Cookiejar()
-    handler = urllib.request.HTTPCookieProcessor(cookie)
-    opener = urllib.request.build_opener(handler)
-    response = opener.open('http://www.17k.com/chapter/2933095/36699279.htm')
-    for item in cookie:
-        print(item.name+'='+ item.value)
-
-    cookie.save(filename=filename.ignore_discard=True,ignore_expires=True)
