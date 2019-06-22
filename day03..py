cb5e66ad239cbdcac1cd2a2969bdc7fb6c965fca
Wangqian550 <51935916+Wangqian550@users.noreply.github.com>
2019-06-22 18:06:55 +0800

    Add files via upload

---
+++ day03..py
@@ -0,0 +1,20 @@
+import requests
+
+headers = {
+ 'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+}
+url = 'http://www.image-net.org/api/text/imagenet.synset.geturls?wnid=n02127808'
+response = requests.get(url)
+w = response.text
+lines = w.split('\r\n')
+for url in lines:
+    try:
+        response = requests.get(url,timeout=2)
+        content = response.content
+        conten2 = str(content)
+        if '\\x' in conten2:
+            print(conten2)
+    except Exception as q:
+        print(q)
+        with open('%s,jpg','wb') as f:
+            f.write(w.content)
